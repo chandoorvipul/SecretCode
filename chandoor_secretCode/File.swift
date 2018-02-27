de6e03e2be851d61a07e9bf89a7dbd45e736e1b1
Chandoor <s530459@ch112015m.nwmissouri.edu>
2018-02-27 11:37:16 -0600

    intialized

---
+++ chandoor_secretCode/File.swift
@@ -0,0 +1,22 @@
+//
+//  CodeWord.swift
+//  chandoor_secretCode
+//
+//  Created by Chandoor,Vipul on 2/27/18.
+//  Copyright © 2018 Chandoor,Vipul. All rights reserved.
+//
+
+import Foundation
+
+class CodeWord {
+    var Symbols :[String]
+    var Count : Int
+    var Code : [String]
+    var Guess : [String] = []
+    var onSymbol : String
+    var Attempts : Int
+    var Status : String
+    
+    init
+    
+}
